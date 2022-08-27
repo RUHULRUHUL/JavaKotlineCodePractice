25633a01781b541ca4cd67b0968b5b04ea5d37d6
RUHUL <ruhulamin122017@gmail.com>
2022-08-27 20:25:42 +0600

    java Basic Code

--- src/Java/DSAlgorithm.java
+++ src/Java/DSAlgorithm.java
@@ -1,2 +1,138 @@
-package Java;public class DSAlgorithm {
+package Java;
+
+public class DSAlgorithm {
+    public static void main(String[] args) {
+        //DSArrayOperation();
+        DSSorting();
+    }
+
+    private static void DSSorting() {
+        BubbleSort();
+    }
+
+    private static void BubbleSort() {
+        int[] array = {12, 2, 5, 3, 10};
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    //swap two array number in array location
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array : ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    private static void DSArrayOperation() {
+        Traversing();
+        insertion();
+        Deletion();
+        SearchItem();
+        updateItem();
+
+    }
+
+    private static void updateItem() {
+        int UpdateIndex = 3;
+        int updateValue = 100;
+
+        int[] array = new int[15];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == UpdateIndex) {
+                array[UpdateIndex] = updateValue;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("item: [" + i + "] = " + array[i]);
+        }
+    }
+
+    private static void SearchItem() {
+        int ITemSearch = 3;
+
+        int[] array = new int[15];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == ITemSearch) {
+                System.out.println("Have an Item Position : [ " + array[i] + " ]");
+            }
+        }
+
+    }
+
+    private static void Deletion() {
+        int RemovedIndex = 3;
+        int size = 5;
+
+
+        int[] array = new int[15];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        for (int i = RemovedIndex; i < size; i++) {
+            array[i] = array[i + 1];
+        }
+
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+
+    }
+
+    private static void insertion() {
+
+        int insertValue = 10;
+        int insertIndex = 4;
+        int size = 5;
+
+
+        int[] array = new int[15];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+
+        for (int i = size; i >= insertIndex; i--) {
+            array[i] = array[i - 1];
+        }
+        array[insertIndex] = insertValue;
+
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+
+    private static void Traversing() {
+        int[] array = {1, 2, 3, 4, 5};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
--- src/Java/Hello.java
+++ src/Java/Hello.java
@@ -1,8 +1,337 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Java");
+package Java;
 
-        String name = "Example";
-        System.out.println(name);
+import com.sun.deploy.util.StringUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Hello {
+
+    public static Scanner scanner;
+
+    public static void main(String[] args) {
+
+        init();
+        // switchFunction();
+        // forLoop();
+        // WhileLoop();
+        // doWhileLoop();
+        // ClassExample();
+        // inharitance();
+        // OverLoadingMethod();
+        //array();
+        //list();
+        //calculateTheStringLength();
+        // arrayMerge();
+        //OneArrayConvertTwoArray();
+        //RepeatedNumberRemovedFromArray();
+        //StringArrayInsert();
+        //StringCheckHaveNumber();
+        //DuplicatedItemRemoved();
+        swapTwoNumber();
+
+
+    }
+
+    private static void swapTwoNumber() {
+        int a = 5;
+        int b = 6;
+        int temp;
+        System.out.println(" a " + a);
+        System.out.println(" b " + b);
+
+
+        a = b;
+        b = a;
+
+        System.out.println();
+
+        System.out.println("Swap Two Number: ");
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+
+    }
+
+    private static void DuplicatedItemRemoved() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 10, 1, 2, 2, 3, 10, 3, 3, 4, 5, 5));
+        System.out.println("ArrayList with duplicates: " + list);
+
+        List<Integer> newList = list.stream().distinct().collect(Collectors.toList());
+
+        System.out.println("ArrayList with duplicates removed: " + newList);
+    }
+
+    private static void StringCheckHaveNumber() {
+        String input = "Ban1234";
+        boolean isDigit = false;
+
+        char[] chars = input.toCharArray();
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                System.out.println("Digit");
+                isDigit = true;
+                break;
+
+            } else {
+                System.out.println("Character");
+            }
+
+        }
+
+        if (isDigit) {
+            System.out.println("Digit Not Allow");
+        } else {
+            System.out.println("Welcome ");
+        }
+
+
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static void StringArrayInsert() {
+        String commaSeparatedStr = "Hello,have,a,nice,day";
+        String[] strArray = null;    //empty string array
+        //converting using String.split() method with comma as a delimiter
+        strArray = commaSeparatedStr.split(",");
+        //printing the converted string array
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+    }
+
+    private static void RepeatedNumberRemovedFromArray() {
+        String input = "1,2,3,4,5,6,7,8,9,10,1,3,4,8,9";
+        String[] array = null;
+
+        array = input.split(",");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("String converted array: " + array[i]);
+        }
+
+        System.out.println();
+
+        HashSet<String> hashSet = new HashSet<>(Arrays.asList(array));
+
+        for (String s : hashSet) {
+            System.out.println("hashset " + s);
+
+        }
+
+
+    }
+
+    private static void OneArrayConvertTwoArray() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int counterKeyPosition = 0;
+
+        int dividedArraySize = array.length / 2;
+        int[] array1 = new int[dividedArraySize];
+        int[] array2 = new int[dividedArraySize];
+
+
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = array[i];
+            counterKeyPosition++;
+        }
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println("array 1 " + array1[i]);
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = array[counterKeyPosition];
+            counterKeyPosition++;
+
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println("array 2 " + array2[i]);
+        }
+
+
+    }
+
+    private static void arrayMerge() {
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {6, 7, 8, 9, 10};
+        int[] array3 = new int[array1.length + array2.length];
+
+        int counter = array1.length;
+
+        for (int i = 0; i < array1.length; i++) {
+            array3[i] = array1[i];
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            array3[counter] = array2[i];
+            counter = counter + 1;
+        }
+
+        for (int i = 0; i < array3.length; i++) {
+            System.out.println("item- " + array3[i]);
+        }
+    }
+
+    private static void calculateTheStringLength() {
+        String countryName = "Bangladesh";
+        int totalCharacter = 0;
+        for (char c : countryName.toCharArray()) {
+
+            totalCharacter = totalCharacter + 1;
+        }
+        System.out.println("total Length " + totalCharacter);
+    }
+
+
+    private static void list() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("List Value = " + list.get(i));
+        }
+
+    }
+
+    private static void array() {
+        int a[] = {33, 3, 4, 5};
+        int[] cloneArray = a.clone();
+
+        for (int i = 0; i < cloneArray.length; i++) {
+            System.out.println("Clone Array " + cloneArray[i]);
+        }
+
+
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 1;
+        array[2] = 1;
+        array[3] = 1;
+        array[4] = 1;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(array[i]);
+        }
+
+        anonymousArray(new int[]{1, 2, 3, 4, 5});
+    }
+
+    private static void anonymousArray(int[] ints) {
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println("AnonymousArray " + ints[i]);
+        }
+
+    }
+
+    private static void OverLoadingMethod() {
+    }
+
+
+    private static void inharitance() {
+        Student student = new Student("Rahul");
+        student.display();
+        System.out.println(student.Department);
+        student.CollageName();
+    }
+
+    private static void ClassExample() {
+        Student student = new Student("Rahul");
+        student.display();
+    }
+
+    private static void doWhileLoop() {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+    }
+
+    private static void WhileLoop() {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    private static void init() {
+        if (scanner == null) {
+            scanner = new Scanner(System.in);
+        }
+    }
+
+    private static void forLoop() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+
+    private static void switchFunction() {
+        System.out.print("Given a Number: ");
+        int a = scanner.nextInt();
+
+        switch (a) {
+            case 5: {
+                System.out.println("value " + a);
+                break;
+            }
+            default: {
+                System.out.println("Not match Value");
+            }
+        }
+    }
+
+
+    public static class Student extends Department {
+
+        String name;
+
+        public Student(String name) {
+            this.name = name;
+        }
+
+        void display() {
+            System.out.println("Student Name " + name);
+        }
+
+        @Override
+        public void CollageName() {
+            super.CollageName();
+        }
+    }
+
+    public static class Department {
+        public String Department = "Computer";
+
+        public void CollageName() {
+            String collageName = "TPI";
+            System.out.println("Collage Name " + collageName);
+        }
+
+    }
+
+    public static class Calculation {
+        public int add(int a, int b) {
+            return a + b;
+
+        }
+
+        public int add(int a, int b, int c) {
+            return a + b + c;
+        }
     }
 }
